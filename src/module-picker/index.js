9fa5435c2c4bdda524b206394a03f8dedfc1481f
Keagan Kuehn <kuehnkeag@ecs.vuw.ac.nz>
2017-12-09 11:57:26 +1300

    Added back button for module selector

--- src/module-picker/index.js
+++ src/module-picker/index.js
@@ -5,6 +5,9 @@ import Modal from 'react-modal';
 import Loader from 'react-jspm-loader';
 import Select from 'react-select';
 
+import BackIcon from 'react-icons/lib/io/ios-arrow-back';
+
+
 import NpmSearch from './npm-search';
 import MultipleExports from './multiple-exports';
 import ModuleChecker from './module-checker';
@@ -48,6 +51,12 @@ export default class ModulePicker extends Component {
       console.log(selected);
    }
 
+   _renderBackButton(val) {
+      return (
+         <BackIcon style={{position : 'absolute', left : '10px', top : '10px', height : '30px', width : '30px', fontSize : '24px'}} onClick={() => {this.setState({view : val})}}/>
+      );
+   }
+
    _renderModalInternals(){
       if(this.state.view == 'module'){
          return (
@@ -60,27 +69,34 @@ export default class ModulePicker extends Component {
          );
       }else if(this.state.view == 'module-parser'){
          return (
-            <ModuleChecker module={this.state.module} onChecked={(test, c) => {
-               if(test){
-                  this.setState({
-                     view: 'submodules',
-                     submodules: c
-                  });
-               }else{
-                  this.setState({
-                     view: 'props',
-                     module: c,
-                  });
-               }
-            }}/>
+            <div>
+               {this._renderBackButton('module')}
+               <ModuleChecker module={this.state.module} onChecked={(test, c) => {
+                  if(test){
+                     this.setState({
+                        view: 'submodules',
+                        submodules: c
+                     });
+                  }else{
+                     this.setState({
+                        view: 'props',
+                        module: c,
+                     });
+                  }
+               }}/>
+            </div>
          );
       }else if(this.state.view == 'submodules'){    
          return (
-            <MultipleExports module={this.state.submodules} onSubmit={this._addComponent.bind(this)}/>
+            <div>
+               {this._renderBackButton('module-parser')}
+               <MultipleExports module={this.state.submodules} onSubmit={this._addComponent.bind(this)}/>
+            </div>
          );
       }else{
          return (
             <div>
+               {this._renderBackButton('module')}
                <h2>{this.state.module.name}</h2>
                <PropEditor component={this.state.module.Component} onSubmit={this._addComponent.bind(this)}/> 
             </div>
@@ -92,6 +108,7 @@ export default class ModulePicker extends Component {
       return (
          <Modal
             isOpen={this.state.open}
+            style={{position : 'relative'}}
          >
          {this._renderModalInternals()} 
 
